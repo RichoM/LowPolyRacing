8d1746f872ad4df86001653657b254adf12e0d06
RichoM <richi.moran@gmail.com>
2021-06-09 19:52:58 -0300

    A few changes to the car so that it accelerates faster

--- car.gd
+++ car.gd
@@ -1,7 +1,7 @@
 extends VehicleBody
 
-const MAX_SPEED = 500
-const MAX_STEERING = 0.35
+const MAX_SPEED = 250
+const MAX_STEERING = 0.25
 const ACCELERATION = 0.75
 
 
@@ -11,12 +11,12 @@ func _process(delta):
 	elif Input.is_action_pressed("ui_down"):
 		engine_force = -ACCELERATION * MAX_SPEED
 	else: 
-		engine_force = 0
+		engine_force *= 0.75
 	
-	var target_steering = 0	
+	var target_steering = 0
 	if Input.is_action_pressed("ui_left"):
 		target_steering = MAX_STEERING
 	elif Input.is_action_pressed("ui_right"):
 		target_steering = -MAX_STEERING
 	
-	steering = lerp(steering, target_steering, 3 * delta)
+	steering = lerp(steering, target_steering, 5 * delta)
