15886a17bd370a925b2befa8640045909274cbd8
RichoM <richi.moran@gmail.com>
2021-06-09 23:06:59 -0300

    Improved decoration and added invisible walls to prevent the car from leaving the track

--- tree.gd
+++ tree.gd
@@ -8,6 +8,8 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	scale_object_local(Vector3(1, rand_range(0.25, 1.5), 1))
+	var w = rand_range(1, 1.5)
+	var h = rand_range(0.25, 1.5)
+	scale_object_local(Vector3(w, h, w))
 
 
