35c2c783624d7f5e67e4b94b855b8949554c9472
RichoM <richi.moran@gmail.com>
2021-06-10 15:00:07 -0300

    Improved car control and changed tires so that they appear to move correctly

--- car.gd
+++ car.gd
@@ -1,15 +1,15 @@
 extends VehicleBody
 
-const MAX_SPEED = 250
+const MAX_SPEED = 750
 const MAX_STEERING = 0.25
 const ACCELERATION = 0.75
 
 
 func _process(delta):
 	if Input.is_action_pressed("ui_up"):
-		engine_force = ACCELERATION * MAX_SPEED
-	elif Input.is_action_pressed("ui_down"):
 		engine_force = -ACCELERATION * MAX_SPEED
+	elif Input.is_action_pressed("ui_down"):
+		engine_force = ACCELERATION * MAX_SPEED
 	else: 
 		engine_force *= 0.75
 	
@@ -19,5 +19,5 @@ func _process(delta):
 	elif Input.is_action_pressed("ui_right"):
 		target_steering = -MAX_STEERING
 	
-	steering = lerp(steering, target_steering, 2.5 * delta)
+	steering = lerp(steering, target_steering, 4 * delta)
 	
