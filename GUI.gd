1cae5722712bfb0a5fd12d189325a5dcc7e31984
RichoM <richi.moran@gmail.com>
2021-06-12 15:57:18 -0300

    Added a play button that starts the game

--- GUI.gd
+++ GUI.gd
@@ -8,6 +8,7 @@ onready var last_time : Label = $right_col/last_time
 onready var best_time : Label = $right_col/best_time
 onready var record : Label = $record
 onready var record_timer : Timer = $record/Timer
+onready var play_button : Button = $play_button
 
 func _ready():
 	player.connect("new_record", self, "_on_new_record")
@@ -38,3 +39,9 @@ func format(duration):
 	var s = floor((duration/1000)%60)
 	var m = floor((duration/(1000*60))%60)
 	return "%02d:%02d:%02d" % [m, s, ms]
+
+
+func _on_play_button_pressed():
+	player.input_enabled = true
+	play_button.hide()
+	print("PLAY!")
