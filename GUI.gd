a689188703b6fbc12a7ffa0be366a225aa117b3b
RichoM <richi.moran@gmail.com>
2021-06-15 15:15:07 -0300

    Added a simple "turn right/left ahead" sign

--- GUI.gd
+++ GUI.gd
@@ -1,6 +1,7 @@
 extends Control
 
 export (NodePath) onready var player  = get_node(player) as Car
+export (NodePath) onready var turns = get_node(turns) as Spatial
 
 onready var lap_counter : Label = $left_col/lap_counter
 onready var cur_time : Label = $right_col/cur_time
@@ -8,10 +9,15 @@ onready var last_time : Label = $right_col/last_time
 onready var best_time : Label = $right_col/best_time
 onready var record : Label = $record
 onready var record_timer : Timer = $record/Timer
+onready var turn_sign : TextureRect = $turn_sign
+onready var turn_sign_timer : Timer = $turn_sign/Timer
 
 func _ready():
 	player.connect("new_record", self, "_on_new_record")
 	record_timer.connect("timeout", record, "hide")
+	turn_sign_timer.connect("timeout", turn_sign, "hide")
+	for turn in turns.get_children():
+		turn.connect("triggered", self, "_on_turn_triggered")
 	
 func _on_new_record():
 	record.text = "NEW RECORD!\n" + format(player.best_time)
@@ -44,3 +50,10 @@ func _on_play_button_pressed():
 	player.input_enabled = true
 	$overlay.hide()
 	print("PLAY!")
+
+
+func _on_turn_triggered(type):
+	if type == "R": turn_sign.flip_h = false
+	elif type == "L": turn_sign.flip_h = true
+	turn_sign.show()
+	turn_sign_timer.start()
